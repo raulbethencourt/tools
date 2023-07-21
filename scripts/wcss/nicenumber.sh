bc5f8bacb899b1bcd7a42860d2e4ad591c5312e3
raul-bns <raul.bethencourt@bluenot-systems.com>
2023-07-21 23:40:16 +0200

    :hammer: Working in wcss

--- scripts/wcss/nicenumber.sh
+++ scripts/wcss/nicenumber.sh
@@ -5,11 +5,30 @@
 #   echoed to stdout.
 
 nicenumber() {
-    # Note that we assume that '.' is the decimal separator in the INPUT value 
-    #   to this script. The decimal separator in the output value is '.' unless
-    #   specified by the user with the -d flag.
+	# Note that we assume that '.' is the decimal separator in the INPUT value
+	#   to this script. The decimal separator in the output value is '.' unless
+	#   specified by the user with the -d flag.
 
-    integer=$(echo "$1" | cut -d. -f1)
-    decimal=$(echo "$1" | cut -d. -f2)
+	integer=$(echo "$1" | cut -d. -f1) # Left of the decimal
+	decimal=$(echo "$1" | cut -d. -f2) # Right of the decimal
+
+	# Check if number has more than the integer part.
+	if [ "$decimal" != "$1" ]; then
+		# There's a fractional part, so let's include it.
+		result="${DD:= '.'}$decimal"
+	fi
+
+    thousands=$integer
+
+    while [ $thousands -gt 999 ]; do
+        remainder=$(($thousands % 1000))
+
+        # TODO: continue
+
+        echo "$remainder"
+    done
+
+    return
 }
 
+nicenumber 2000000.870
